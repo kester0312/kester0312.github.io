5e7f12f98875de565123d2237a4bcd3b8a17423c
kester0312 <99643504+kester0312@users.noreply.github.com>
2023-12-08 22:51:50 +0800

    big updatev2

---
+++ code/js/copy.js
@@ -0,0 +1,23 @@
+function copyText(element) {
+    var range, selection, worked;
+  
+    if (document.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(element);
+      range.select();
+    } else if (window.getSelection) {
+      selection = window.getSelection();        
+      range = document.createRange();
+      range.selectNodeContents(element);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+    
+    try {
+      document.execCommand('copy');
+      alert('文字已經複製');
+    }
+    catch (err) {
+      alert('文字無法複製');
+    }
+  }
