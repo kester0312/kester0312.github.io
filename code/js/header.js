95fd97faf54c41e7fae311bb06bb6c48705b9177
kester0312 <chanholongkester@gmail.com>
2026-05-30 20:54:14 +0800

    mc

--- code/js/header.js
+++ code/js/header.js
@@ -1,15 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const headerHTML = `
-    <link rel="stylesheet" href="../../code/css/header.css">
-    <header class="site-header" style="padding-top: 8px;padding-left: 10px;">
+    <link rel="stylesheet" href="/code/css/header.css">
+    <header class="site-header" style="padding-top: 8px; padding-bottom: 10px; padding-left: 10px; height: 70px; box-sizing: border-box; display: block; width: 100%;">
       <div class="header-container">
-        <a href="../../index.html" class="logo" title="麥塊藍月光">
-          <img src="../../icon.png" alt="Logo" style="width: auto; height: 55px;">
+        <a href="/index.html" class="logo" title="麥塊藍月光">
+          <img src="/icon.png" alt="Logo" style="width: auto; height: 55px; display: block;">
         </a>
       </div>
     </header>
-    <br>
-    <br>
   `;
   document.body.insertAdjacentHTML("afterbegin", headerHTML);
 });
